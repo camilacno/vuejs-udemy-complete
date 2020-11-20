98f03e7552dc1a0ed50d7a078cfdad5065a3f4dd
Camila Nepomuceno <camila.cno@gmail.com>
2020-11-20 19:18:37 -0300

    Vuejs - 07_Section07_Componentes

---
+++ 01_VueJS_Udemy/07_Section07_Componentes/componente-intro/src/components/Contador.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="contador">
+    <span>{{ contador }}</span>
+    <button @click="somar">+</button>
+    <button @click="subtrair">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contador: 0,
+    };
+  },
+  methods: {
+    somar() {
+      return this.contador++;
+    },
+    subtrair() {
+      return this.contador--;
+    },
+  },
+};
+</script>
+
+<style scoped>
+span {
+  border-bottom: 1px solid #ccc;
+  height: 30px;
+  padding: 5px 25px;
+}
+button {
+  height: 30px;
+  width: 30px;
+  border-radius: 50%;
+  background-color: coral;
+  color: #fff;
+  margin-left: 10px;
+  outline: none;
+}
+</style>
---
+++ 01_VueJS_Udemy/07_Section07_Componentes/componente-intro/src/components/Contadores.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="contadores">
+    <app-contador v-for="i in 5" :key="i" />
+  </div>
+</template>
+
+<script>
+import Contador from '../components/Contador';
+
+export default {
+  components: {
+    'app-contador': Contador,
+  },
+};
+</script>
+
+<style></style>
