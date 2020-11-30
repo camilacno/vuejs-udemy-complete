2f07c360f5acc4af36ff3745ff6590d9bdeeea9b
Camila Nepomuceno <camila.cno@gmail.com>
2020-11-30 11:58:00 -0300

    Vuejs - 16_Section16_Rotas

---
+++ 01_VueJS_Udemy/16_Section16_Rotas/rotas-exercicios/src/components/template/MenuAlt.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="menu">
+    <ul>
+      <router-link
+        to="/"
+        tag="li"
+        active-class="active"
+        exact
+      ><a>Start</a></router-link>
+      <router-link
+        to="/usuario"
+        tag="li"
+        active-class="active"
+      ><a>Users</a></router-link>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
